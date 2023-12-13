5050f2967ddeb931711c1e68ce615dbf2eebae86
isagutierrez12 <148836439+isagutierrez12@users.noreply.github.com>
2023-12-12 21:46:09 -0600

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,20 +1,7 @@
 import os
 
-'''
-Radiador Springs S.A.
-
-1. Ver rutas: En este apartado deben mostrarse al menos 5 rutas en las que
-operará el servicio. Para efectos de este apartado puede crear un arreglo
-unidimensional que almacene las cinco rutas en el formato: “Coronado – San José”. Entre otras.
-
-2. Ver precios: En este apartado debe mostrarse al menos 3 opciones de
-compra de tiquetes. Por ejemplo, “Tiquete sencillo 500 colones”, “Tiquete
-por un día 1000 colones”, etc. Para esto utilice un arreglo
-unidimensional.
-'''
-
 rutas1 = ["Ruta 1: Coronado - San José","Ruta 2: Guadalupe - San José","Ruta 3: Tibas - San José","Ruta 4: San Pedro - San José","Ruta 5: Santa Marta - San José"]
-precios1 = ["Tiquete sencillo: 500 colones", "Tiquete por un día: 1.000 colones", "Tiquete semanal: 6.000 colones", "Tiquete mensual: 20.000 colones"]
+precios1 = ["1. Tiquete sencillo: 500 colones", "2. Tiquete por un día: 1.000 colones", "3. Tiquete semanal: 6.000 colones", "4. Tiquete mensual: 20.000 colones"]
 matriz1 = [["1-1", "1-2"], ["2-1", "2-2"], ["3-1", "3-2"], ["4-1", "4-2"], ["5-1", "5-2"]]
 matriz2 = [["1-1", "1-2"], ["2-1", "2-2"], ["3-1", "3-2"], ["4-1", "4-2"], ["5-1", "5-2"]]
 matriz3 = [["1-1", "1-2"], ["2-1", "2-2"], ["3-1", "3-2"], ["4-1", "4-2"], ["5-1", "5-2"]]
@@ -23,92 +10,110 @@ matriz5 = [["1-1", "1-2"], ["2-1", "2-2"], ["3-1", "3-2"], ["4-1", "4-2"], ["5-1
     
 def control():
     while True:
-        respuesta = int(input("\nDigite la opción que desea: \n 1. Volver al menú principal \n 2. Salir \n"))
-        if respuesta == 1:
+        respuesta = input("\nDigite la opción que desea: \n 1. Volver al menú principal \n 2. Salir \n")
+        if respuesta == "1":
             Menu()
-        elif respuesta == 2:
+        elif respuesta == "2":
             print("\nGracias por utilizar Radiador Springs S.A.")
             quit()
         else:
             print("\nPor favor digite una opción válida")
 
 
-def rutas(rutas1): 
-    for ruta in rutas1:
-        print(ruta)
-    ruta_seleccionada = int(input("Seleccione la ruta\n"))
+def rutas(rutas1):
+    while True:
+        try:
+            for ruta in rutas1:
+                print(ruta)
+            ruta_seleccionada = int(input("\nSeleccione la ruta que desea (1-5):\n"))
 
-    if 1 <= ruta_seleccionada <= 5:
-        return ruta_seleccionada
-    print(rutas[ruta_seleccionada - 1])
-    #Agregar while y opcion de else
+            if 1 <= ruta_seleccionada <= 5:
+                return ruta_seleccionada
+                print(rutas[ruta_seleccionada - 1])
+            else:
+                print("\nPor favor digite una opción válida\n")
+        except:
+            print("\nPor favor digite una opción válida\n")
+        
 
 
 def precios(precios1):
-    for precio in precios1:
-        print(precio)
-    Tiquete_seleccionado = int(input("Seleccione el tiquete\n"))
+    while True:
+        try:
+            for precio in precios1:
+                print(precio)
+            Tiquete_seleccionado = int(input("\nSeleccione el tipo de tiquete que desea (1-4):\n"))
 
-    if 1 <= Tiquete_seleccionado <= 4:
-        return Tiquete_seleccionado
-    print(precios[Tiquete_seleccionado - 1])
-    #Agregar while y opcion de else
+            if 1 <= Tiquete_seleccionado <= 4:
+                return Tiquete_seleccionado
+                print(precios[Tiquete_seleccionado - 1])
+            else:
+                print("\nPor favor digite una opción válida\n")
+        except:
+            print("\nPor favor digite una opción válida\n")
+
 
 def campos(matriz, precio):
     while True:
-        cantidad = int(input("Digite la cantidad de tiquetes que desea: "))
+        try:
+            cantidad = int(input("\nDigite la cantidad de tiquetes que desea (1-10): \n"))
+            if cantidad <= 10:
+                print("\n")
 
-        if cantidad <= 10:
-            for r in range (cantidad):
-            
-                for x in range (0,5):
-                    for y in range (0,2):
-                        print(matriz[x][y],end= " ")
-                    print("\n")
                 
-
-                x = int(input("Seleccione la fila (1-5): "))
-                y = int(input("Seleccione la columna (1-2): "))
-                if 1 <= x <= 5 and 1 <= y <= 2:
-                    x -= 1
-                    y -= 1
-                    if matriz[x][y] != "O":
-                        matriz[x][y] = "O"
-                        print(f"Ha seleccionado el asiento: Fila {x + 1}, Columna {y + 1}")
+                for r in range (0,cantidad):
+                    
+                    for x in range (0,5):
+                        for y in range (0,2):
+                            print(matriz[x][y],end= " ")
+                        print("\n")
+                        
+                    x = int(input("Seleccione la fila que desea (1-5): \n"))
+                    y = int(input("Seleccione la columna que desea (1-2): \n"))
+                    if 1 <= x <= 5 and 1 <= y <= 2:
+                        x -= 1
+                        y -= 1
+                        if matriz[x][y] != "O":
+                            matriz[x][y] = "O"
+                            print(f"\nHa seleccionado el asiento: Fila {x + 1}, Columna {y + 1}\n")
+                        else:
+                            print("El espacio seleccionado ya está ocupado\n")
+                            break
                     else:
-                        print("El espacio seleccionado ya está ocupado")
-                else:
-                    print("Selección de asiento no válida.")
+                        print("Selección de asiento no válida.\n")
 
-            subtotal = cantidad * precio
-            iva = subtotal * 0.13
-            total = subtotal + iva
 
-            archivo = open("Factura.txt","w")
-            archivo.write("\nEl detalle de su compra\n")
-            archivo.write(f"Cantidad de tiquetes comprados: {cantidad}\n")
-            archivo.write(f"Tipo de tiquetes: {precio} colones\n")
-            archivo.write(f"Subtotal: {subtotal} colones\n")
-            archivo.write(f"iva: {iva} colones\n")
-            archivo.write(f"Total: {total} colones\n")
-            archivo.close()
+                subtotal = cantidad * precio
+                iva = subtotal * 0.13
+                total = subtotal + iva
 
-            archivo = open("Control ventas.txt","a")
-            archivo.write("\nEl detalle de su compra\n")
-            archivo.write(f"Cantidad de tiquetes comprados: {cantidad}\n")
-            archivo.write(f"Tipo de tiquetes: {precio} colones\n")
-            archivo.write(f"Subtotal: {subtotal} colones\n")
-            archivo.write(f"iva: {iva} colones\n")
-            archivo.write(f"Total: {total} colones\n")
-            archivo.close()
+                archivo = open("Factura.txt","w")
+                archivo.write("\nEl detalle de su compra\n")
+                archivo.write(f"Cantidad de tiquetes comprados: {cantidad}\n")
+                archivo.write(f"Tipo de tiquetes: {precio} colones\n")
+                archivo.write(f"Subtotal: {subtotal} colones\n")
+                archivo.write(f"IVA: {iva} colones\n")
+                archivo.write(f"Total: {total} colones\n")
+                archivo.close()
 
-            print(f"subtotal: {subtotal}" )
-            print(f"iva: {iva} " )
-            print(f"total: {total}" )
+                archivo = open("Control ventas.txt","a")
+                archivo.write("\nEl detalle de su compra\n")
+                archivo.write(f"Cantidad de tiquetes comprados: {cantidad}\n")
+                archivo.write(f"Tipo de tiquetes: {precio} colones\n")
+                archivo.write(f"Subtotal: {subtotal} colones\n")
+                archivo.write(f"IVA: {iva} colones\n")
+                archivo.write(f"Total: {total} colones\n")
+                archivo.close()
 
-            break
-        else:
-            print("El máximo de tiquetes que puede comprar es 10")
+                print(f"Subtotal: {subtotal}" )
+                print(f"IVA: {iva} " )
+                print(f"Total: {total}" )
+
+                break
+            else:
+                print("El máximo de tiquetes que puede comprar es 10")
+        except:
+            print("\nPor favor digite una opción válida\n")
     
 def espacios(matriz):
     disponibles = 0
@@ -125,20 +130,20 @@ def espacios(matriz):
 
 def Menu():
     while True:
-        opcion = int(input("\nDigite la opción que desea: \n 1. Ver Rutas \n 2. Ver Precios \n 3. Adquirir Tiquetes \n 4. Consultar Cantidad de espacios disponibles \n 5. Salir \n "))
-        if opcion == 1:
+        opcion = input("\nDigite la opción que desea: \n 1. Ver Rutas \n 2. Ver Precios \n 3. Adquirir Tiquetes \n 4. Consultar Cantidad de espacios disponibles \n 5. Salir \n ")
+        if opcion == "1":
             print(f"\nLas rutas disponibles son:")
             for ruta in rutas1:
                 print(ruta)
             control()
 
-        elif opcion == 2:
+        elif opcion == "2":
             print(f"\nLos precios disponibles son:")
             for precio in precios1:
                 print(precio)
             control()
 
-        elif opcion == 3:
+        elif opcion == "3":
             ruta_seleccionada = rutas(rutas1)
             if  ruta_seleccionada == 1:
                 matriz = matriz1
@@ -165,7 +170,7 @@ def Menu():
                 campos(matriz, precio)
             control()
 
-        elif opcion == 4:
+        elif opcion == "4":
             ruta_seleccionada = rutas(rutas1)
             if  ruta_seleccionada == 1:
                 matriz = matriz1
@@ -181,7 +186,7 @@ def Menu():
                 espacios(matriz)
             control()
 
-        elif opcion == 5:
+        elif opcion == "5":
             print("Gracias por utilizar Radiador Springs S.A.")
             break
 
